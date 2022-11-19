5ace1f6149ebc504ffaf647f2ce2c741e4c66446
PERROUAS Thibault <107963463+Pyracantharia@users.noreply.github.com>
2022-11-19 22:12:53 +0100

    update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,20 +1,6 @@
 <?php
-$header = '<header>
+$header = '';
 
-
-
-<button name="nav_menu" class="nav_menu">Menu</button>
-
-
-
-<img src="img/logo.png" alt="logo" class="logo"/>
-
-
-
-
-<button name="signin" class="signin">Se connecter</button>
-
-</header>';
 echo $header;
 
 ?>
