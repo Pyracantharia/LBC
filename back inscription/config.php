c98dcd3641e7d58c54cbc6de011ac1be44f582ec
PERROUAS Thibault <107963463+Pyracantharia@users.noreply.github.com>
2022-11-21 23:23:09 +0100

    test conn

--- back inscription/config.php
+++ back inscription/config.php
@@ -6,14 +6,11 @@
            $bdd = new PDO("mysql:host=CHANGER_HOST_ICI;dbname=CHANGER_DB_NAME;charset=utf8;port=3306", "CHANGER_LOGIN", "CHANGER_PASS");
            
          */
- 
-
-    try {
-        $this->connection = new mysqli(localhost:3306, xuereert_lbc, 5XqExJE!;3G(, xuereert_lbc);
-     
-        if ( mysqli_connect_errno()) {
-            throw new Exception("Could not connect to database.");   
-        }
-    } catch (Exception $e) {
-        throw new Exception($e->getMessage());   
-    }       
+    try 
+    {
+        $bdd = new PDO("mysql:host=localhost;dbname=xuereert_lbc;charset=utf8", "xuereert_lbc", "5XqExJE!;3G(");
+    }
+    catch(PDOException $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
