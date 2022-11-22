e2c6e597af942047dabb5acd5c62f7eedf525da6
PERROUAS Thibault <107963463+Pyracantharia@users.noreply.github.com>
2022-11-22 21:14:58 +0100

    application du back sur page inscription

--- back inscription/inscription.php
+++ back inscription/inscription.php
@@ -1,123 +1,142 @@
 <!DOCTYPE html>
-    <html lang="en">
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="NoS1gnal" />
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Inscription</title>
+</head>
+
+<body>
+    <div class="login-form">
+        <?php
+        if (isset($_GET['reg_err'])) {
+            $err = htmlspecialchars($_GET['reg_err']);
+
+            switch ($err) {
+                case 'success':
+        ?>
+                    <div class="alert alert-success">
+                        <strong>Succès</strong> inscription réussie !
+                    </div>
+                <?php
+                    break;
+
+                case 'password':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Erreur</strong> mot de passe différent
+                    </div>
+                <?php
+                    break;
+
+                case 'email':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Erreur</strong> email non valide
+                    </div>
+                <?php
+                    break;
+                case 'tel_bad':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Erreur</strong> mauvais format de telephone (ex: 06 00 00 00 00)
+                    </div>
+                <?php
+                    break;
+
+                case 'email_length':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Erreur</strong> email trop long
+                    </div>
+                <?php
+                    break;
+
+                case 'pseudo_length':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Erreur</strong> pseudo trop long
+                    </div>
+                <?php
+                case 'already':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Erreur</strong> compte deja existant
+                    </div>
+        <?php
+
+            }
+        }
+        ?>
+        <!DOCTYPE html>
+        <html lang="en">
+
         <head>
             <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="author" content="NoS1gnal"/>
-
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-            <title>Inscription</title>
+            <title>Connexion | Inscription</title>
+            <link rel="stylesheet" href="/Contact/contact.css">
         </head>
+
         <body>
-        <div class="login-form">
-            <?php 
-                if(isset($_GET['reg_err']))
-                {
-                    $err = htmlspecialchars($_GET['reg_err']);
-
-                    switch($err)
-                    {
-                        case 'success':
-                        ?>
-                            <div class="alert alert-success">
-                                <strong>Succès</strong> inscription réussie !
-                            </div>
-                        <?php
-                        break;
-
-                        case 'password':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> mot de passe différent
-                            </div>
-                        <?php
-                        break;
-
-                        case 'email':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> email non valide
-                            </div>
-                        <?php
-                        break;
-                        case 'tel_bad':
-                            ?>
-                                <div class="alert alert-danger">
-                                    <strong>Erreur</strong> mauvais format de telephone (ex: 06 00 00 00 00)
-                                </div>
-                            <?php
-                            break;
-
-                        case 'email_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> email trop long
-                            </div>
-                        <?php 
-                        break;
-
-                        case 'pseudo_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> pseudo trop long
-                            </div>
-                        <?php 
-                        case 'already':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> compte deja existant
-                            </div>
-                        <?php 
-
-                    }
-                }
-                ?>
-            
-            <form action="inscription_traitement.php" method="post">
-                <h2 class="text-center">Inscription</h2>       
-                <div class="form-group">
-                    <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off">
+            <div class="container">
+                <div id="connexion">
+                    <h1 class="title">De Retour !</h1>
+                    <p class="paragraphe">
+                        Veuillez entrer vos détails de connexion et démarrez votre journée sur LBC M2L
+                    </p>
+                    <a href="/Connexion/Connexion.html" class="btn-link connexion">Se connecter</a>
                 </div>
-                <div class="form-group">
-                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+                <div id="inscription">
+                    <h1 class="title">Créer un compte</h1>
+                    <p class="paragraphe">
+                        Veuillez remplir tous les champs *
+                    </p>
+
+
+
+                    <form action="inscription_traitement.php" method="post" class="formulaire">
+
+                        <div class="group-form">
+                            <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off" placeholder="Entrez votre nom complet *">
+                            <div class="icon-user"></div>
+                        </div>
+
+                        <div class="group-form">
+                            <input input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off" placeholder="Entrez votre mail *">
+                            <div class="icon-mail"></div>
+                        </div>
+
+                        <div class="group-form">
+                            <input type="tel" name="tel" class="form-control" placeholder="telephone" required="required" autocomplete="off" placeholder="Entrez votre numéro ">
+                            <div class="icon-tel"></div>
+                        </div>
+
+                        <div class="group-form">
+                            <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off" placeholder="Mot de passe *">
+                            <div class="icon-password"></div>
+                        </div>
+
+                        <div class="group-form">
+                            <input type="password" name="password_retype" class="form-control" placeholder="Re-tapez le mot de passe" required="required" autocomplete="off" placeholder="Confirmer votre mot de passe *">
+                            <div class="icon-password"></div>
+                        </div>
+
+                        <div class="group-form">
+                            <button type="submit" class="inscription">Inscription</button>
+                        </div>
+                    </form>
+
+
+
                 </div>
-                <div class="form-group">
-                    <input type="tel" name="tel" class="form-control" placeholder="telephone" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password_retype" class="form-control" placeholder="Re-tapez le mot de passe" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block">Inscription</button>
-                </div>   
-            </form>
-        </div>
-        <style>
-            .login-form {
-                width: 340px;
-                margin: 50px auto;
-            }
-            .login-form form {
-                margin-bottom: 15px;
-                background: #f7f7f7;
-                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-                padding: 30px;
-            }
-            .login-form h2 {
-                margin: 0 0 15px;
-            }
-            .form-control, .btn {
-                min-height: 38px;
-                border-radius: 2px;
-            }
-            .btn {        
-                font-size: 15px;
-                font-weight: bold;
-            }
-        </style>
+            </div>
         </body>
-</html>
+
+        </html>
