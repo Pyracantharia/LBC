03bc3f8fed3014d2257f68f16ea46706a45504d9
Pyracantharia <thibaultperrouas8@gmail.com>
2022-11-28 11:41:04 +0100

    Variabiliser le header et le footer

--- index.php
+++ index.php
@@ -1,21 +1,51 @@
 <?php
-
+require 'template/header.php';
 ?>
+<main>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+<div class="schearch">
+
+    <div class="schearch shadow p-3 bg-body rounded">
+
+        <div>
+            <h1 class="form-check form-check-inline">Voir les offres</h1>
+            <!--
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+                <label class="form-check-label" for="inlineRadio1">Offres</label>
+            </div>
+            
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                <label class="form-check-label" for="inlineRadio2">Demandes</label>
+            </div>
+            -->
+        </div>
+
+        <nav class="navbar btn-light rounded">
+            <div class="container-fluid">
+                <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                    <option selected>Catégories</option>
+                    <option value="1">Natation</option>
+                    <option value="2">Cyclisme</option>
+                    <option value="3">Tennis</option>
+                </select>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Que recherchez-vous ?" aria-label="Search">
+                    <input class="form-control me-2 " type="search" placeholder="Saisissez une ville" aria-label="Search">
+                    <button class="btn btn-light btn-outline-success" type="submit">Rechercher</button>
+
+                </form>
+            </div>
+        </nav>
+
+    </div>
+</div>
+
+
+</main>
+
+
 <?php
-echo "Hello World";
+require 'template/footer.php';
 ?>
-
-
-</body>
-</html>
---
+++ template/footer.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+
+<div class="container">
+        <footer class="py-5 footer">
+            <div class="footer-color bg-body shadow">
+                <div class="row">
+                    <div class="">
+                        <h5>Section</h5>
+                        <ul class="nav flex-column">
+                            
+
+                            <li class="nav-item mb-2">
+                                <a href="/About/about.html" class="nav-link p-0 text-muted">
+                                    A Propos
+                                    
+
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+
+            <p>© 2022 Company, Inc. All rights reserved.</p>
+
+
+        </footer>
+    </div>
+</body>
+
+</html>
