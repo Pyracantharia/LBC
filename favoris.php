0e8a4417ffad6f72d24ffa8bbcca0c7ef3c04999
PERROUAS Thibault <107963463+Pyracantharia@users.noreply.github.com>
2022-11-28 21:02:14 +0100

    favoris en php

---
+++ favoris.php
@@ -0,0 +1,35 @@
+<?php
+require 'template/head.php';
+?>
+
+<link rel="stylesheet" href="/css/favoris_style.css">
+
+<?php
+require 'template/header.php';
+?>
+
+
+
+<main>
+<br>
+        <div class="schearch">
+
+            <div class="schearch shadow p-3 bg-body rounded">
+                <h1>Retrouvez vos annonces sauvegardées</h1>
+                <br>
+                <img src="..." class="rounded-1" alt="...">
+                <h2>Titre de l'annonce</h2>
+                <button type="button" class="btn btn-secondary">Enlever des favoris</button>
+
+
+                
+
+            </div>
+        </div>
+
+
+    </main>
+
+    <?php
+require 'template/footer.php';
+?>
