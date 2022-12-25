38b9b5a258c1e333aef4e2f279811349350c0a2f
PERROUAS Thibault <107963463+Pyracantharia@users.noreply.github.com>
2022-12-25 17:40:25 +0100

    ins

--- back inscription/inscription.php
+++ back inscription/inscription.php
@@ -73,18 +73,17 @@
             }
         }
         ?>
-        <!DOCTYPE html>
-        <html lang="en">
 
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Connexion | Inscription</title>
-            <link rel="stylesheet" href="/Contact/contact.css">
-        </head>
 
-        <body>
+<?php
+require 'template/head.php';
+?>
+<link rel="stylesheet" href="/Contact/contact.css">
+
+
+<?php
+require 'template/header.php';
+?>
             <div class="container">
                 <div id="connexion">
                     <h1 class="title">De Retour !</h1>
